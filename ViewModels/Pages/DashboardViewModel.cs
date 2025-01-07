561fe3b9161d627b50ad9d2de5bbba248ae74801
tabutabu98 <songyechan.kr@gmail.com>
2025-01-07 21:11:12 +0900

    feat : MVVM Data Binding

--- ViewModels/Pages/DashboardViewModel.cs
+++ ViewModels/Pages/DashboardViewModel.cs
@@ -3,12 +3,16 @@
     public partial class DashboardViewModel : ObservableObject
     {
         [ObservableProperty]
+        private string? text = string.Empty;
+
+        [ObservableProperty] // 어트리뷰트(Java 의 어노테이션과 비슷)
         private int _counter = 0;
 
         [RelayCommand]
         private void OnCounterIncrement()
         {
-            Counter++;
+            //++Counter;
+            this.Text = "Clicked!!";
         }
     }
 }
--- Views/Pages/DashboardPage.xaml.cs
+++ Views/Pages/DashboardPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Start_WPF.ViewModels.Pages;
+﻿using System.ComponentModel;
+using System.Windows.Media;
+using Start_WPF.ViewModels.Pages;
 using Wpf.Ui.Controls;
 
 namespace Start_WPF.Views.Pages
@@ -12,7 +14,27 @@ namespace Start_WPF.Views.Pages
             ViewModel = viewModel;
             DataContext = this;
 
+            // ViewModel 의 속성 변경 이벤트 함수 호출
+            ViewModel.PropertyChanged += ViewModel_PropertyChanged; // 속성 변경 이벤트
+
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// DashboardViewModel 속성 변경 시 실행
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            switch(e.PropertyName)
+            {
+                case "Text":
+                    this.clickmeBtn.Background = new SolidColorBrush(Colors.White);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
